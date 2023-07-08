632633c6752e1f55d4f0647c10a52f00da77855f
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-08 09:53:23 +0000

    Import Quote component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quote />
     </div>
   );
 }
