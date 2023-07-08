3b8a7df16e076cbb8b233981dbe5b1cea6255e99
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-08 06:39:37 +0000

    Create child component Button

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+
+const Button = ({ title, className }) => (<button type="button" className={className}>{title}</button>);
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  className: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Button;
