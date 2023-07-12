31c9f22124ad4931a3daa46d8f624983ff796aca
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-12 20:37:18 +0000

    Refactor jsx structure

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -5,15 +5,13 @@ const Quote = () => {
 
   return (
     <div id="quotes" className="page-content">
-      <div className="quotes-container">
-        <div id="display-quote">
-          <p id="quote-text" className={`${JSON.stringify(data) === '{}' ? 'd-none' : ''}`}>
-            <i>{data?.quote || ''}</i>
-            <b>{data?.author || ''}</b>
-          </p>
-          <p id="loading-message" className={`${!loading ? 'd-none' : ''}`}>{ loading ? 'Loading Quote...' : '' }</p>
-          <p id="error-message" className={`${!error ? 'd-none' : ''}`}>{ error || '' }</p>
-        </div>
+      <div id="display-quote">
+        <p id="quote-text" className={`${JSON.stringify(data) === '{}' ? 'd-none' : ''}`}>
+          <i>{data?.quote || ''}</i>
+          <b>{data?.author || ''}</b>
+        </p>
+        <p id="loading-message" className={`${!loading ? 'd-none' : ''}`}>{ loading ? 'Loading Quote...' : '' }</p>
+        <p id="error-message" className={`${!error ? 'd-none' : ''}`}>{ error || '' }</p>
       </div>
     </div>
   );
