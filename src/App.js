d4b67aefd151d3f6f25bcdd829ef12ca05cb2431
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-12 20:40:15 +0000

    Refactor App component to use react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import './App.css';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Quote />
-      <Calculator />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route path="/" element={<Home />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route path="quote" element={<Quote />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
