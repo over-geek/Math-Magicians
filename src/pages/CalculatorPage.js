aad511154ac77508d504d34476b9129046f63ddb
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-12 21:07:31 +0000

    Adding pages to App

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,11 @@
+import './CalculatorPage.css';
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <div className="calc-page-container">
+    <h2>Let&apos;s do some Math!</h2>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
