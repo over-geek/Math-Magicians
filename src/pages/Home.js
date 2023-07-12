e0686884337f02d79c7647817a09ecd491ac4be4
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-12 20:38:04 +0000

    Add home component

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to our page!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam malesuada ipsum lorem,
+        a lacinia neque gravida aliquam.Vivamus scelerisque arcu ac tellus lacinia mollis.
+        Sed imperdiet, purus eget cursus imperdiet, ligula est venenatis odio,
+        sit amet aliquam felis tellus sed nisi. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.Maecenas non nunc sit amet massa iaculis ultrices.
+        Sed semper lacus id eros scelerisque malesuada. Curabitur pellentesque,
+        arcu nec eleifend molestie, turpis dolor porttitor elit, eu tempus leo erat vitae arcu.
+        Cras sollicitudin interdum metus sit amet pretium. Integer ornare odio blandit,
+        pretium nisi et, consequat nunc. Mauris ut placerat dolor.
+        Mauris ut sapien in tellus lobortis ornare ut at tellus.
+        Curabitur sollicitudin rhoncus diam vel volutpat. Phasellus nec imperdiet sem,
+        vel ultrices erat. Nunc lacus diam, lacinia eu mi vitae, tristique dapibus turpis.
+        <br />
+        <br />
+        In sagittis nunc sed turpis pharetra mattis nec a ante. Maecenas vel cursus arcu.
+        Vestibulum eu risus a nibh congue fringilla.
+        Duis sit amet sapien ut felis tempus efficitur in non metus.
+        Suspendisse finibus ex quis eleifend placerat. Quisque eleifend viverra massa at facilisis.
+        Cras varius at tortor facilisis mollis.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
